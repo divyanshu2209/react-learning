dbc4f3a2e238f7d6b2a7e7e22ecb1f8d79f2b8de
Divyanshu Rai <divyanshurai2209@gmail.com>
2024-08-11 01:38:58 +0530

    learning to make custom react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App : raju</h1>
+    </div>
+  )
+}
+const anotherElement = (
+  <a href="https://google.com" target = '_blank'>Visit Google</a>
+)
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google',
+  anotherElement
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    reactElement
 )
